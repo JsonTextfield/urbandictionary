ec0d3e21d4a0acfe5a5aca2974b088dafb9d9475
jasonbromfield <jasonhowlook@gmail.com>
2025-05-01 21:04:44 -0400

    close suggestions on search

--- composeApp/src/commonMain/kotlin/com/jsontextfield/urbandictionary/ui/App.kt
+++ composeApp/src/commonMain/kotlin/com/jsontextfield/urbandictionary/ui/App.kt
@@ -1,8 +1,6 @@
 package com.jsontextfield.urbandictionary.ui
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.imePadding
@@ -10,7 +8,6 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.rememberLazyListState
-import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.rounded.ArrowBack
@@ -19,7 +16,6 @@ import androidx.compose.material.icons.rounded.Shuffle
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -95,7 +91,10 @@ fun App() {
                                 SearchBar(
                                     value = mainViewModel.searchText,
                                     onValueChanged = mainViewModel::onSearchTextChanged,
-                                    onSearch = { mainViewModel.onListTypeChanged(ListType.SEARCH) },
+                                    onSearch = {
+                                        showAutocomplete = false
+                                        mainViewModel.onListTypeChanged(ListType.SEARCH)
+                                    },
                                     onTextCleared = { mainViewModel.onListTypeChanged(ListType.HOME) },
                                     modifier = Modifier.focusRequester(focusRequester)
                                 )
